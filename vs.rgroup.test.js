20c41e965ae1751099f3ba669ac5de3d0c6df202
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-04-28 22:36:16 +0300

    UT: RSite iterator, removeConstraints and addConstraint

--- vs.rgroup.test.js
+++ vs.rgroup.test.js
@@ -11,9 +11,27 @@
  * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
  * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
  ***************************************************************************/
-require('./vs.rgroup.test');
-require('./vs.tau_en.test');
-require('./vs.cdx.test');
-require('./vs.basic.test');
-require('./vs.buffer_string_load_iterate.test.js');
-require('./vs.logger.test');
+
+/* declaration of modules  */
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var local = path.join.bind(path, __dirname);
+
+var indigo = require("../indigo-node/indigo");
+indigo.setOption("treat-x-as-pseudoatom", true)
+indigo.setOption("ignore-stereochemistry-errors", true)
+
+var testRSite = function () {
+	var query = indigo.loadQueryMolecule("[OH]C1C([OH])C([*:1])OC([*:2])C1[OH]");
+	console.log(query.smiles());
+	for (var rsite of query.iterateRSites())
+	{
+		rsite.removeConstraints("rsite");
+		rsite.addConstraint("atomic-number", "12");
+	}
+	console.log(query.smiles());
+}
+
+testRSite();
+	
