d6a923e295e3be2b1da2de8ef1484280bed958a7
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-05-03 12:48:52 +0300

    add UT: Substructure with either and bidirectional mode :aromatize(); :checkBadValence(); :checkBadValence(); :indigo.substructureMatcher :match :indigo.unserialize :serialize :optimize

---
+++ vs.substruct.test.js
@@ -0,0 +1,51 @@
+/****************************************************************************
+ * Copyright (C) 2015-2016 EPAM Systems
+ * 
+ * This file is part of Indigo-Node binding.
+ * 
+ * This file may be distributed and/or modified under the terms of the
+ * GNU General Public License version 3 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.md  included in the
+ * packaging of this file.
+ * 
+ * This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+ * WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ ***************************************************************************/
+
+/* declaration of modules  */
+var assert = require('assert');
+var path = require('path');
+var fs = require('fs');
+var local = path.join.bind(path, __dirname);
+
+var indigo = require("../indigo-node/indigo");
+
+var checkHasMatchMol = function (indigo, m, q) {
+	q.aromatize();
+	m.checkBadValence();
+	m.checkBadValence();
+	var matcher = indigo.substructureMatcher(m);
+	assert(matcher.match(q) != null);
+	
+	var m2 = indigo.unserialize(m.serialize());
+	var matcher2 = indigo.substructureMatcher(m2)
+	assert(matcher2.match(q) != null);
+	q.optimize();
+	assert(matcher.match(q) != null);
+	assert(matcher2.match(q) != null)
+};
+
+var checkHasMatch = function (indigo, targetName, queryName) {
+	console.log(targetName + " " + queryName);
+	var q = indigo.loadQueryMoleculeFromFile(local("../indigo-node/"+queryName));
+	var m = indigo.loadMoleculeFromFile(local("../indigo-node/"+targetName));
+	checkHasMatchMol(indigo, m, q);
+};
+
+console.log("***** Substructure with either and bidirectional mode *****");
+indigo.setOption("stereochemistry-bidirectional-mode", true);
+checkHasMatch(indigo, "molecules/either1.mol", "molecules/either1.mol");
+checkHasMatch(indigo, "molecules/either1.mol", "molecules/either2.mol");
+checkHasMatch(indigo, "molecules/either1.mol", "molecules/either1_query.mol");
+
+
