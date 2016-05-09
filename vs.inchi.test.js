631b0a8286d2c8846a6cbbbee8ddf55bbbdbb616
Dmitry Sigaev <Dmitry_Sigaev@epam.com>
2016-05-09 07:59:01 +0300

    add UT: indigo_inchi.getInchi indigo_inchi.getWarning

--- vs.inchi.test.js
+++ vs.inchi.test.js
@@ -28,3 +28,5 @@ console.log(indigo_inchi.version());
 console.log("*** Basic *** ");
 var m = indigo_inchi.loadMolecule("InChI=1S/C10H20N2O2/c11-7-1-5-2-8(12)10(14)4-6(5)3-9(7)13/h5-10,13-14H,1-4,11-12H2");
 console.log(m.canonicalSmiles());
+console.log(indigo_inchi.getInchi(m));
+console.log(indigo_inchi.getWarning());
