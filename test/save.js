334884fd9193336e1fb3ba76055d07edc2d6bef7
Nikita_Ryzhov <Nikita_Ryzhov@epam.com>
2017-01-16 15:26:25 +0300

    update tests 'save'

--- test/save.js
+++ test/save.js
@@ -13,109 +13,117 @@
  ***************************************************************************/
 
 /* declaration of modules  */
-var assert = require('assert');
-var path = require('path');
-var fs = require('fs');
-var local = path.join.bind(path, __dirname);
+let test = require('tape');
+let tmp = require('tmp');
 
-var Indigo = require("../indigo").Indigo;
-var indigo = new Indigo();
+let assert = require('assert');
+let path = require('path');
+let fs = require('fs');
+let local = path.join.bind(path, __dirname);
 
-var testMultipleSave = function (smifile, iterfunc, issmi){
+let Indigo = require("../indigo").Indigo;
+let indigo = new Indigo();
+
+let tmpDir = tmp.dirSync({ template: local('/tmp-XXXXXX'), unsafeCleanup: true });
+
+let testMultipleSave = function (/*test*/ t, smifile, iterfunc, issmi){
 	console.log("TESTING " + path.parse(smifile).name);
-	var sdfout = indigo.writeFile(local("structures.sdf"));
-	var cmlout = indigo.writeFile(local("structures.cml"));
-	var rdfout = indigo.writeFile(local("structures.rdf"));
-	var smiout = indigo.writeFile(local("structures.smi"));
+	let sdfout = indigo.writeFile(tmpDir.name + ("/structures.sdf"));
+	let cmlout = indigo.writeFile(tmpDir.name + ("/structures.cml"));
+	let rdfout = indigo.writeFile(tmpDir.name + ("/structures.rdf"));
+	let smiout = indigo.writeFile(tmpDir.name + ("/structures.smi"));
 	rdfout.rdfHeader();
 	cmlout.cmlHeader();
-	for (var item of iterfunc(smifile)) {
-		var exc = false;
-		try {
-			item.countAtoms();
-			item.smiles();
-		}
-		catch (e) {
-			console.log(item.index() + e.message);
-			if (issmi)
-				console.log(item.rawData());
-			exc = true;
-		}
-		if (!exc) {
-			// item.clearCisTrans();
-			for (var bond of item.iterateBonds()) {
-				if ((bond.topology() == Indigo.RING) && (bond.bondOrder() == 2)) {
-					bond.resetStereo();
-				}
-			}
-			try {
-				item.markEitherCisTrans();
-			}
-			catch (e) {
-				console.log(item.index() + ' (while markEitherCisTrans) : ' + e.message);
-				if (issmi)
-					console.log(item.rawData());
-				continue;
-			}
-			if (issmi) {
-				item.setName('structure-' + item.index() + ' ' + item.rawData());
-			} else {
-				item.setName('structure-' + item.index());
-			}
-			item.setProperty("NUMBER", item.index().toString());
-			cmlout.cmlAppend(item);
-			smiout.smilesAppend(item);
-			item.layout();
-			indigo.setOption("molfile-saving-mode", "2000");
-			sdfout.sdfAppend(item);
-			indigo.setOption("molfile-saving-mode", "3000");
-			rdfout.rdfAppend(item);
-		}
-	}
-	cmlout.cmlFooter();
-	sdfout.close();
-	cmlout.close();
-	rdfout.close();
-	smiout.close();
+	t.doesNotThrow(() => {
+        for (let item of iterfunc(smifile)) {
+            let exc = false;
+            try {
+                item.countAtoms();
+                item.smiles();
+            } catch (e) {
+                if (issmi)
+                    throw new Error(item.rawData());
+                exc = true;
+            }
+            if (!exc) {
+                // item.clearCisTrans();
+                for (let bond of item.iterateBonds()) {
+                    if ((bond.topology() == Indigo.RING) && (bond.bondOrder() == 2)) {
+                        bond.resetStereo();
+                    }
+                }
+                try {
+                    item.markEitherCisTrans();
+                } catch (e) {
+                    console.log(item.index() + ' (while markEitherCisTrans) : ' + e.message);
+                    if (issmi)
+                        throw new Error(item.rawData());
+                    continue;
+                }
+                if (issmi) {
+                    item.setName('structure-' + item.index() + ' ' + item.rawData());
+                } else {
+                    item.setName('structure-' + item.index());
+                }
+                item.setProperty("NUMBER", item.index().toString());
+                cmlout.cmlAppend(item);
+                smiout.smilesAppend(item);
+                item.layout();
+                indigo.setOption("molfile-saving-mode", "2000");
+                sdfout.sdfAppend(item);
+                indigo.setOption("molfile-saving-mode", "3000");
+                rdfout.rdfAppend(item);
+            }
+        }
+        cmlout.cmlFooter();
+        sdfout.close();
+        cmlout.close();
+        rdfout.close();
+        smiout.close();
+	}, null, 'check right file');
+    sdfout.close();
+    cmlout.close();
+    rdfout.close();
+    smiout.close();
 
-	var cmliter = indigo.iterateCMLFile(local("structures.cml"));
-	var sdfiter = indigo.iterateSDFile(local("structures.sdf"));
-	var rdfiter = indigo.iterateRDFile(local("structures.rdf"));
-	var smiiter = indigo.iterateSmilesFile(local("structures.smi"));
-	var idx = 1;
-	while (sdf = sdfiter.next().value) {
-		cml = cmliter.next().value;
-		rdf = rdfiter.next().value;
-		smi = smiiter.next().value;
+	let cmliter = indigo.iterateCMLFile(local("structures.cml"));
+	let sdfiter = indigo.iterateSDFile(local("structures.sdf"));
+	let rdfiter = indigo.iterateRDFile(local("structures.rdf"));
+	let smiiter = indigo.iterateSmilesFile(local("structures.smi"));
+	let idx = 1;
+	let sdf;
+    t.doesNotThrow(() => {
+        while (sdf = sdfiter.next().value) {
+            let cml = cmliter.next().value;
+            let rdf = rdfiter.next().value;
+            let smi = smiiter.next().value;
 
-		console.log(sdf.index() + ' ' + sdf.name());
-		sdf.resetSymmetricCisTrans();
-		rdf.resetSymmetricCisTrans();
-		try {
-			var cs1 = sdf.canonicalSmiles();
-			var cs2 = rdf.canonicalSmiles();
-			var cs3 = smi.canonicalSmiles();
-			var cs4 = cml.canonicalSmiles();
-		}
-		catch (e) {
-			console.log(e.message);
-			continue;
-		}
-		console.log(cs1);
-		console.log(cs2);
-		console.log(cs3);
-		console.log(cs4);
-		if (cs2 != cs1)
-			console.log("MISMATCH");
-		if (cs3 != cs1)
-			console.log("MISMATCH");
-		if (cs4 != cs1)
-			console.log("MISMATCH");
-		idx += 1;
-	}
-}
+            sdf.resetSymmetricCisTrans();
+            rdf.resetSymmetricCisTrans();
+            try {
+                let cs1 = sdf.canonicalSmiles();
+                let cs2 = rdf.canonicalSmiles();
+                let cs3 = smi.canonicalSmiles();
+                let cs4 = cml.canonicalSmiles();
+                if (cs2 != cs1 || cs3 != cs1 || cs4 != cs1)
+                    console.log("MISMATCH");
+            }
+            catch (e) {
+                console.log(e.message);
+                continue;
+            }
+            idx += 1;
+        }
+    }, null, 'check iterate');
+};
 
-testMultipleSave(local("fixtures/helma.smi"), indigo.iterateSmilesFile.bind(indigo), true);
-testMultipleSave(local("fixtures/chemical-structures.smi"), indigo.iterateSmilesFile.bind(indigo), true);
-testMultipleSave(local("fixtures/pubchem_7m_err.sdf"), indigo.iterateSDFile.bind(indigo), false);
-testMultipleSave(local("fixtures/acd2d_err.sdf"), indigo.iterateSDFile.bind(indigo), false);
+test('\n#### - SAVE test - ####\n', function (t) {
+	t.plan(4);
+    testMultipleSave(t, local("fixtures/helma.smi"), indigo.iterateSmilesFile.bind(indigo), true);
+    testMultipleSave(t, local("fixtures/chemical-structures.smi"), indigo.iterateSmilesFile.bind(indigo), true);
+    testMultipleSave(t, local("fixtures/pubchem_7m_err.sdf"), indigo.iterateSDFile.bind(indigo), false);
+    testMultipleSave(t, local("fixtures/acd2d_err.sdf"), indigo.iterateSDFile.bind(indigo), false);
+
+    tmpDir.removeCallback();
+});
+
